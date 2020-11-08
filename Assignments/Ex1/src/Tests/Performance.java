b0664afb1c1044644d4aa6a4f61c62026b5c6a4f
Amit-Sheer Cohen <amitcohen95@gmail.com>
2020-11-08 12:22:12 +0200

    added performance test suit but not coded yet

--- Assignments/Ex1/src/Tests/Performance.java
+++ Assignments/Ex1/src/Tests/Performance.java
@@ -2,9 +2,16 @@ package Tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 public class Performance extends BaseTest {
-    @Test
-    void maxSizeGraphTimeCheck(){
-        graphCreator();
-    }
+//    @Test
+//    void maxSizeGraphTimeCheck(){
+//        int maxNodes = (int)Math.pow(10,6);
+//        int maxEdges = maxNodes*10;
+//        Timestamp timestamp = Timestamp.from(Instant.now());
+//        graphCreator(1,maxNodes,maxEdges,100 );
+//        Timestamp timestampAfter = Timestamp.from(Instant.now());
+//    }
 }
