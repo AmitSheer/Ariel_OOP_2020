dcf77255039d871e1b14f8a25c46981949927d1b
Amit <amitcohen95@gmail.com>
2020-11-10 13:55:24 +0200

    aa

--- Assignments/Ex1/src/Tests/Performance.java
+++ Assignments/Ex1/src/Tests/Performance.java
@@ -33,41 +33,54 @@ public class Performance extends BaseTest {
         System.out.println(graph.edgeSize());
         System.out.println(graph.nodeSize());
     }
+
+    /**
+     * creates all nodes. when more then 10 nodes are created
+     * it will start creating edges between the current created node to a random 10 other nodes.
+     * after will create edges from first 10 nodes to other nodes, and the first node
+     * will be connected to other node
+     * @param seed for the random
+     * @param size number of nodes to create
+     * @param edges number of edges
+     * @param weight
+     */
     public void graphCreatorForPerformanceTests2(int seed, int size,int edges, int weight){
         graph = new WGraph_DS();
         Random _rnd = new Random(seed);
         Random rndWeight = new Random(weight);
         int key;
         graph.addNode(0);
+        //create all of the nodes
         for (int i = 1; i < size; i++) {
             graph.addNode(i);
+            //when more then 10 nodes exist
             if(i>9) {
                 for (int j = 0; j < 10; j++){
                     key = _rnd.nextInt(i);
+                    //check if has an edge or if the random key is same as current node key
                     while (graph.hasEdge(i, key) || i == key) {
-                      //  key1 = _rnd.nextInt(i + 1);
                         key = _rnd.nextInt(i);
                     }
                     graph.connect(i, key, rndWeight.nextDouble());
                 }
             }
             else{
-                    //key1 = _rnd.nextInt(i + 1);
                     key = _rnd.nextInt(i);
                     while (graph.hasEdge(i, key) || i == key) {
-                      //  key1 = _rnd.nextInt(i + 1);
                         key = _rnd.nextInt(i);
                     }
                     graph.connect(i, key, rndWeight.nextDouble());
                 }
         }
+        //adds 9 edges to first 10 nodes
+        int a = _rnd.nextInt(size);
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 9; j++) {
                 key = _rnd.nextInt(size);
-                while (graph.hasEdge(i, key) || i == key) {
+                while (graph.hasEdge(a, key) || a == key) {
                     key = _rnd.nextInt(size);
                 }
-                graph.connect(i, key, rndWeight.nextDouble());
+                graph.connect(a, key, rndWeight.nextDouble());
             }
         }
         key = _rnd.nextInt(size);
